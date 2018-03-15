e49af7c87899123a71147bc352b1cec6d2e872f5
sanjpradhan <sanjaykpradhan@gmail.com>
2018-03-15 01:20:33 +0000

    html files

--- public/assets/scripts/pages/dashboard.js
+++ public/assets/scripts/pages/dashboard.js
@@ -1,11 +1,11 @@
-$( document ).ready(function() {
-    
+$(document).ready(function() {
+
     // CounterUp Plugin
     $('.counter').counterUp({
         delay: 10,
         time: 1000
     });
-    
+
     setTimeout(function() {
         toastr.options = {
             closeButton: true,
@@ -16,57 +16,57 @@ $( document ).ready(function() {
         };
         toastr.success('Checkout settings menu on left!', 'Welcome to Modern!');
     }, 1800);
-    
-    var flot2 = function () {
 
-		// We use an inline data source in the example, usually data would
-		// be fetched from a server
+    var flot2 = function() {
 
-		var data = [],
-			totalPoints = 100;
-        
-		function getRandomData() {
+        // We use an inline data source in the example, usually data would
+        // be fetched from a server
 
-			if (data.length > 0)
-				data = data.slice(1);
+        var data = [],
+            totalPoints = 100;
 
-			// Do a random walk
+        function getRandomData() {
 
-			while (data.length < totalPoints) {
+            if (data.length > 0)
+                data = data.slice(1);
 
-				var prev = data.length > 0 ? data[data.length - 1] : 50,
-					y = prev + Math.random() * 10 - 5;
+            // Do a random walk
 
-				if (y < 0) {
-					y = 0;
-				} else if (y > 100) {
-					y = 100;
-				}
+            while (data.length < totalPoints) {
 
-				data.push(y);
-			}
+                var prev = data.length > 0 ? data[data.length - 1] : 50,
+                    y = prev + Math.random() * 10 - 5;
 
-			// Zip the generated y values with the x values
+                if (y < 0) {
+                    y = 0;
+                } else if (y > 100) {
+                    y = 100;
+                }
 
-			var res = [];
-			for (var i = 0; i < data.length; ++i) {
-				res.push([i, data[i]])
-			}
+                data.push(y);
+            }
 
-			return res;
-		}
+            // Zip the generated y values with the x values
 
-		var plot2 = $.plot("#flotchart2", [ getRandomData() ], {
-			series: {
-				shadowSize: 0	// Drawing is faster without shadows
-			},
-			yaxis: {
-				min: 0,
-				max: 100
-			},
-			xaxis: {
-				show: false
-			},
+            var res = [];
+            for (var i = 0; i < data.length; ++i) {
+                res.push([i, data[i]])
+            }
+
+            return res;
+        }
+
+        var plot2 = $.plot("#flotchart2", [getRandomData()], {
+            series: {
+                shadowSize: 0 // Drawing is faster without shadows
+            },
+            yaxis: {
+                min: 0,
+                max: 100
+            },
+            xaxis: {
+                show: false
+            },
             colors: ["#22BAA0"],
             legend: {
                 show: false
@@ -82,78 +82,101 @@ $( document ).ready(function() {
                 content: "Y: %y",
                 defaultTheme: false
             }
-		});
+        });
 
-		function update() {
-			plot2.setData([getRandomData()]);
+        function update() {
+            plot2.setData([getRandomData()]);
 
-			plot2.draw();
-			setTimeout(update, 30);
-		}
+            plot2.draw();
+            setTimeout(update, 30);
+        }
 
-		update();
-        
+        update();
+
     };
-    
+
     flot2();
-    var flot1 = function () {
-        var data = [[0, 65], [1, 60], [2, 80], [3, 80], [4, 55], [5, 55], [6, 40]];
-        var data2 = [[0, 30], [1, 50], [2, 40], [3, 20], [4, 85], [5, 25], [6, 90]];
-        var dataset =  [
-            {
-                data: data,
-                color: "#f25656",
-                lines: {
-                    show: true,
-                    fill: 0,
-                },
-                shadowSize: 0,
-            }, {
-                data: data,
-                color: "#fff",
-                lines: {
-                    show: false,
-                },
-                points: {
-                    show: true,
-                    fill: true,
-                    radius: 4,
-                    fillColor: "#f25656",
-                    lineWidth: 2
-                },
-                curvedLines: {
-                    apply: false,
-                },
-                shadowSize: 0
-            }, {
-                data: data2,
-                color: "rgba(34,186,160,1)",
-                lines: {
-                    show: true,
-                    fill: 0,
-                },
-                shadowSize: 0,
-            },{
-                data: data2,
-                color: "#fff",
-                lines: {
-                    show: false,
-                },
-                curvedLines: {
-                    apply: false,
-                },
-                points: {
-                    show: true,
-                    fill: true,
-                    radius: 4,
-                    fillColor: "rgba(34,186,160,1)",
-                    lineWidth: 2
-                },
-                shadowSize: 0
-            }
+    var flot1 = function() {
+        var data = [
+            [0, 65],
+            [1, 60],
+            [2, 80],
+            [3, 80],
+            [4, 55],
+            [5, 55],
+            [6, 40]
         ];
-        
-        var ticks = [[0, "1"], [1, "2"], [2, "3"], [3, "4"], [4, "5"], [5, "6"], [6, "7"], [7, "8"]];
+        var data2 = [
+            [0, 30],
+            [1, 50],
+            [2, 40],
+            [3, 20],
+            [4, 85],
+            [5, 25],
+            [6, 90]
+        ];
+        var dataset = [{
+            data: data,
+            color: "#f25656",
+            lines: {
+                show: true,
+                fill: 0,
+            },
+            shadowSize: 0,
+        }, {
+            data: data,
+            color: "#fff",
+            lines: {
+                show: false,
+            },
+            points: {
+                show: true,
+                fill: true,
+                radius: 4,
+                fillColor: "#f25656",
+                lineWidth: 2
+            },
+            curvedLines: {
+                apply: false,
+            },
+            shadowSize: 0
+        }, {
+            data: data2,
+            color: "rgba(34,186,160,1)",
+            lines: {
+                show: true,
+                fill: 0,
+            },
+            shadowSize: 0,
+        }, {
+            data: data2,
+            color: "#fff",
+            lines: {
+                show: false,
+            },
+            curvedLines: {
+                apply: false,
+            },
+            points: {
+                show: true,
+                fill: true,
+                radius: 4,
+                fillColor: "rgba(34,186,160,1)",
+                lineWidth: 2
+            },
+            shadowSize: 0
+        }];
+
+        var ticks = [
+            [0, "1"],
+            [1, "2"],
+            [2, "3"],
+            [3, "4"],
+            [4, "5"],
+            [5, "6"],
+            [6, "7"],
+            [7, "8"]
+        ];
 
         var plot1 = $.plot("#flotchart1", dataset, {
             series: {
@@ -189,11 +212,22 @@ $( document ).ready(function() {
                 defaultTheme: false
             }
         });
-        
+
     };
-    
+
     flot1();
-    
+
     $(".live-tile").liveTile();
-    
+
+
+    //hack 
+    $("#range_02").ionRangeSlider({
+        min: 100,
+        max: 1000,
+        from: 550
+    });
+
+
+
+
 });
